63a6c7634c55309c20e1492194aecd3e7f096428
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-11-18 10:38:33 +0800

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -1,12 +1,7 @@
-
-
-
-mkfs.ext4 -L boot /dev/sda1
-mkfs.ext4 -L main /dev/sda2
-mkfs.ext4 -L var /dev/sda3
-mkfs.ext4 -L home /dev/sda5
-mkfs.ext4 -L tmp /dev/sda6
-mkfs.ext4 -L varlog /dev/sda7
-mkfs.ext4 -L vartmp /dev/sda8
-mkfs.ext4 -L varlogaudit /dev/sda9
-mkfs.ext4 -L devshm /dev/sda10
+mkfs.ext4 -L tmp /dev/mmcblk0p3
+mkfs.ext4 -L var /dev/mmcblk0p5
+mkfs.ext4 -L vartmp /dev/mmcblk0p6
+mkfs.ext4 -L varlog /dev/mmcblk0p7
+mkfs.ext4 -L varlogaudit /dev/mmcblk0p8
+mkfs.ext4 -L home /dev/mmcblk0p9
+mkfs.ext4 -L devshm /dev/mmcblk0p10
